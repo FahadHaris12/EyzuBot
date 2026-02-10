c765788ee9cfc04829415348ef1d97abdc144991
FahadHaris12 <oggygaiming@gmail.com>
2026-02-10 17:59:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.listen(3000, () => {
 // Create Minecraft bot
 const bot = mineflayer.createBot({
   host: "vnxace.aternos.me", // Example: eyzuserver.aternos.me
-  port: 61163, // Java port
+  port: 25565, // Java port
   username: "EyzuBot", // Bot name
 });
 
